5fecc43d53385befb63f0ebae1ccaa99944a917a
Jul Jen <juliajen.prog@gmail.com>
2018-07-23 17:57:05 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,18 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+     self.all.includes(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
+  end
+
+  def self.sailors
+  end
+
+  def self.talented_seafarers
+  end
+
+  def self.non_sailors
+  end
+
+
 end
