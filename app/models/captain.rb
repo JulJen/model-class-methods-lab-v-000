e64d6d14fe7ffeab0dbd57fa5f714515b7cbe4a8
Jul Jen <juliajen.prog@gmail.com>
2018-07-23 18:19:52 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,22 +2,22 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-     self.all.includes(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
+     all.includes(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
   end
 
   def self.sailors
-    self.all.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+    all.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
   def self.motorboaters
-    self.all.includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+    all.includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
   end
 
   def self.talented_seafarers
     sailors = self.sailors.pluck(:id)
     motorboaters = self.motorboaters.pluck(:id)
-    
-    self.where("id IN (?)", sailors & motorboaters)
+
+    all.where("id IN (?)", sailors & motorboaters)
   end
 
   def self.non_sailors
